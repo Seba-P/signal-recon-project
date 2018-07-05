40d62e79b722a06cf26952285f19b0b0b0ea3032
Sebastian Pisklak <sebastian.pisklak@gmail.com>
2018-07-05 11:26:18 +0200

    modified Octave scripts

--- matlab/process_signal.m
+++ matlab/process_signal.m
@@ -21,35 +21,39 @@ function [ filt_sig_n, filt_lvls_n, ver_lvls_n ] = process_signal(signal, lvls, 
             for iter = 2:n_iter
                 filt_sig(iter, p:p+p_range-1)   = conv(fir_resp, filt_sig(iter-1, p:p+p_range-1))(fir_delay:end-fir_tail);
                 filt_lvls(iter, p:p+p_range-1)  = conv(fir_resp, filt_lvls(iter-1, p:p+p_range-1))(fir_delay:end-fir_tail);
-                ver_lvls(iter, p:p+p_range-1)   = verify_lvls(filt_lvls(iter, p:p+p_range-1), filt_lvls(iter-1, p:p+p_range-1), sig_lvls);
+                ver_lvls(iter, p:p+p_range-1)   = verify_lvls(filt_lvls(iter, p:p+p_range-1), lvls(p:p+p_range-1), sig_lvls);
             end
 
             p = p + p_range;
         end
     elseif strcmp(tolower(filtering_type), 'piecewise-preserved') % not working properly - TBD
-        filt_sig(1, 1:fir_R)  = conv(fir_resp, [zeros(1, fir_R) signal(1:fir_R)'])(fir_delay+fir_R:end-fir_tail);
-        filt_lvls(1, 1:fir_R) = conv(fir_resp, [zeros(1, fir_R) lvls(1:fir_R)])(fir_delay+fir_R:end-fir_tail);
-        ver_lvls(1, 1:fir_R)  = verify_lvls(filt_lvls(1, 1:fir_R), lvls(1:fir_R), sig_lvls);
+        pres_len = fir_R;
+        disc_len = 0;
+        appr_len = pres_len - disc_len;
+
+        filt_sig(1, 1:pres_len)  = conv(fir_resp, [zeros(1, pres_len) signal(1:pres_len)'])(fir_delay+pres_len:end-disc_len-fir_tail);
+        filt_lvls(1, 1:pres_len) = conv(fir_resp, [zeros(1, pres_len) lvls(1:pres_len)])(fir_delay+pres_len:end-disc_len-fir_tail);
+        ver_lvls(1, 1:pres_len)  = verify_lvls(filt_lvls(1, 1:pres_len), lvls(1:pres_len), sig_lvls);
 
         for iter = 2:n_iter
-            filt_sig(iter, 1:fir_R)   = conv(fir_resp, filt_sig(iter-1, 1:fir_R))(fir_delay:end-fir_tail);
-            filt_lvls(iter, 1:fir_R)  = conv(fir_resp, filt_lvls(iter-1, 1:fir_R))(fir_delay:end-fir_tail);
-            ver_lvls(iter, 1:fir_R)   = verify_lvls(filt_lvls(iter, 1:fir_R), filt_lvls(iter-1, 1:fir_R), sig_lvls);
+            filt_sig(iter, 1:pres_len)   = conv(fir_resp, [zeros(1, pres_len) filt_sig(iter-1, 1:pres_len)])(fir_delay+pres_len:end-disc_len-fir_tail);
+            filt_lvls(iter, 1:pres_len)  = conv(fir_resp, [zeros(1, pres_len) filt_lvls(iter-1, 1:pres_len)])(fir_delay+pres_len:end-disc_len-fir_tail);
+            ver_lvls(iter, 1:pres_len)   = verify_lvls(filt_lvls(iter, 1:pres_len), lvls(1:pres_len), sig_lvls);
         end
 
-        p = fir_R + 1;
+        p = appr_len + 1;
         while p < length(signal)
-            p_range = min(fir_R, length(signal)-p+1);
-            p_pres  = p-fir_R;
+            p_range = min(pres_len, length(signal)-p+1);
+            p_pres  = p-pres_len;
 
-            filt_sig(1, p:p+p_range-1)  = conv(fir_resp, [signal(p_pres:p_pres+fir_R-1)' signal(p:p+p_range-1)'])(fir_delay+fir_R:end-fir_tail);
-            filt_lvls(1, p:p+p_range-1) = conv(fir_resp, [lvls(p_pres:p_pres+fir_R-1) lvls(p:p+p_range-1)])(fir_delay+fir_R:end-fir_tail);
+            filt_sig(1, p:p+p_range-1)  = conv(fir_resp, [signal(p_pres:p_pres+pres_len-1)' signal(p:p+p_range-1)'])(fir_delay+pres_len:end-disc_len-fir_tail);
+            filt_lvls(1, p:p+p_range-1) = conv(fir_resp, [lvls(p_pres:p_pres+pres_len-1) lvls(p:p+p_range-1)])(fir_delay+pres_len:end-disc_len-fir_tail);
             ver_lvls(1, p:p+p_range-1)  = verify_lvls(filt_lvls(1, p:p+p_range-1), lvls(p:p+p_range-1), sig_lvls);
 
             for iter = 2:n_iter
-                filt_sig(iter, p:p+p_range-1)   = conv(fir_resp, filt_sig(iter-1, p:p+p_range-1))(fir_delay:end-fir_tail);
-                filt_lvls(iter, p:p+p_range-1)  = conv(fir_resp, filt_lvls(iter-1, p:p+p_range-1))(fir_delay:end-fir_tail);
-                ver_lvls(iter, p:p+p_range-1)   = verify_lvls(filt_lvls(iter, p:p+p_range-1), filt_lvls(iter-1, p:p+p_range-1), sig_lvls);
+                filt_sig(iter, p:p+p_range-1)   = conv(fir_resp, [filt_sig(iter-1, p_pres:p_pres+pres_len-1) filt_sig(iter-1, p:p+p_range-1)])(fir_delay+pres_len:end-disc_len-fir_tail);
+                filt_lvls(iter, p:p+p_range-1)  = conv(fir_resp, [filt_lvls(iter-1, p_pres:p_pres+pres_len-1) filt_lvls(iter-1, p:p+p_range-1)])(fir_delay+pres_len:end-disc_len-fir_tail);
+                ver_lvls(iter, p:p+p_range-1)   = verify_lvls(filt_lvls(iter, p:p+p_range-1), lvls(p:p+p_range-1), sig_lvls);
             end
 
             p = p + p_range;
@@ -62,7 +66,7 @@ function [ filt_sig_n, filt_lvls_n, ver_lvls_n ] = process_signal(signal, lvls, 
         for iter = 2:n_iter
             filt_sig(iter, 1:end)   = conv(fir_resp, filt_sig(iter-1, 1:end))(fir_delay:end-fir_tail);
             filt_lvls(iter, 1:end)  = conv(fir_resp, filt_lvls(iter-1, 1:end))(fir_delay:end-fir_tail);
-            ver_lvls(iter, 1:end)   = verify_lvls(filt_lvls(iter, 1:end), filt_lvls(iter-1, 1:end), sig_lvls);
+            ver_lvls(iter, 1:end)   = verify_lvls(filt_lvls(iter, 1:end), lvls, sig_lvls);
         end
     end
 
