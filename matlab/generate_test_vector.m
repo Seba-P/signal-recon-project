1ebcdbca2ef46977757e19dfa03db137a6d680b4
Seba-P <sebastian.pisklak@gmail.com>
2019-08-19 13:55:48 +0200

    test vectors are now generated in .tgz archive

--- matlab/generate_test_vector.m
+++ matlab/generate_test_vector.m
@@ -14,10 +14,11 @@ function samples_struct = generate_test_vector(vector_name, lvl0, sig_lvls, n_it
         init_guess = 0;
     end
 
-    mkdir('../vectors/', vector_name);
+    cd('../vectors');
+    mkdir('./', vector_name);
 
     % Dump simulation parameters
-    fd = fopen(sprintf('../vectors/%s/readme.txt', vector_name), 'w');
+    fd = fopen(sprintf('%s/readme.txt', vector_name), 'w');
 
     fprintf(fd, '# Test vector "%s".\n', vector_name);
     fprintf(fd, '# Generated on %s.\n', datestr(clock()));
@@ -29,7 +30,7 @@ function samples_struct = generate_test_vector(vector_name, lvl0, sig_lvls, n_it
     fclose(fd);
 
     % Generate configuration for POCS engine
-    fd = fopen(sprintf('../vectors/%s/config.txt', vector_name), 'w');
+    fd = fopen(sprintf('%s/config.txt', vector_name), 'w');
 
     fprintf(fd, '##### PARAMETERS #####\n', n_iter);
     fprintf(fd, '\n');
@@ -53,14 +54,22 @@ function samples_struct = generate_test_vector(vector_name, lvl0, sig_lvls, n_it
     fclose(fd);
 
     % Generate set of samples
-    save_samples(sprintf('../vectors/%s/samples.txt', vector_name), samples, 'text');
+    save_samples(sprintf('%s/samples.txt', vector_name), samples, 'text');
 
     % Generate original & reconstructed signals if applicable
     if (!isempty(signal_original))
-        save_signal(sprintf('../vectors/%s/signal_original.txt', vector_name), signal_original, 'text');
+        save_signal(sprintf('%s/signal_original.txt', vector_name), signal_original, 'text');
     end
 
     if (!isempty(signal_reconstructed))
-        save_signal(sprintf('../vectors/%s/signal_reconstructed.txt', vector_name), signal_reconstructed, 'text');
+        save_signal(sprintf('%s/signal_reconstructed.txt', vector_name), signal_reconstructed, 'text');
     end
+
+    % Archive test_vector and clean up the files
+    tar(sprintf('%s.tgz', vector_name), sprintf('%s/*', vector_name));
+
+    confirm_recursive_rmdir(0);
+    rmdir(sprintf('%s', vector_name), 's');
+
+    cd('../tests');
 end
